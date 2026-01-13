f68902b87b9d17e93f177d512208f30f97aaa979
Lucas Silva <50602386+lucas-silva-GH@users.noreply.github.com>
2026-01-12 23:08:25 -0300

    Fiz a fun main, contendo os devidos teste que achei pertinente fazer, como adição de novos alunos, colocar alunos repitidos e etc

--- desafio.kt
+++ desafio.kt
@@ -41,6 +41,47 @@ data class Formacao(val nome: String, private val conteudos: MutableList<Conteud
 
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    // Criando usuários
+    val user1 = Usuario(1512, "Lucas Silva")
+    val user2 = Usuario(1513, "João Pereira")
+    val user3 = Usuario(1513, "Lucas Silva")
+    val user4 = Usuario(1512, "Lucas Silva")
+
+    // Criando formação
+    val formacaoKotlin = Formacao(
+        nome = "Formação Kotlin Developer",
+        nivel = Nivel.INTERMEDIARIO
+    )
+    
+    val formacaoFlutter = Formacao(
+        nome = "Formação em Flutter/Dart",
+        nivel = Nivel.DIFICIL
+    )
+
+    // Adicionando conteúdos
+    formacaoKotlin.adicionarConteudo(
+        ConteudoEducacional("Introdução ao Kotlin", 50, Nivel.BASICO)
+    )
+    formacaoKotlin.adicionarConteudo(
+        ConteudoEducacional("POO com Kotlin", 80, Nivel.INTERMEDIARIO)
+    )
+    formacaoKotlin.adicionarConteudo(
+        ConteudoEducacional("Kotlin Avançado", 120, Nivel.DIFICIL)
+    )
+
+    // Listando conteúdos
+    formacaoKotlin.listarConteudos()
+
+    // Matriculando usuários
+    formacaoKotlin.matricular(user1)
+    formacaoFlutter.matricular(user1)
+    formacaoKotlin.matricular(user2)
+    formacaoKotlin.matricular(user1) // tentativa duplicada
+    formacaoKotlin.matricular(user3)
+    formacaoKotlin.matricular(user4)
+
+    // Listando inscritos
+    formacaoKotlin.listarInscritos()
+    formacaoFlutter.listarInscritos()
 }
