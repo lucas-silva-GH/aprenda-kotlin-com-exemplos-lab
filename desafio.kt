9dfab3df72086ea9666844da11916ce0dcde0710
Lucas Silva <50602386+lucas-silva-GH@users.noreply.github.com>
2026-01-12 23:07:05 -0300

    Implementei novas funções dentro da class Formacao (ex: listarInscritos, adicionarConteudo, matricular, listarConteudos), além passar como parâmetro a classe Nivel.

--- desafio.kt
+++ desafio.kt
@@ -6,14 +6,39 @@ data class Usuario (val matricula: Int, val nome: String)
 
 data class ConteudoEducacional(val nome: String, val duracao: Int, val nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+data class Formacao(val nome: String, private val conteudos: MutableList<ConteudoEducacional> = mutableListOf(), 
+                    val nivel: Nivel) {
+	
+    private val inscritos = mutableListOf<Usuario>()
+    
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+    }
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (inscritos.contains(usuario)) {
+            println("O usuário ${usuario.nome} já está matriculado na formação '$nome'.")
+        } else {
+            inscritos.add(usuario)
+            println("Usuário ${usuario.nome} matriculado com sucesso na formação '$nome'.")
+        }
+    }
+    
+    fun listarInscritos() {
+        println("\nAlunos inscritos em '$nome':")
+        inscritos.forEachIndexed { i, u ->
+            println("${i + 1}. ${u.nome} (${u.matricula})")
+        }
+    }
+
+    fun listarConteudos() {
+        println("\nConteúdos da formação '$nome':")
+        conteudos.forEachIndexed { i, c ->
+            println("${i + 1}. ${c.nome} - ${c.duracao}min - Nível: ${c.nivel}")
+        }
     }
 }
+
 
 fun main() {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
